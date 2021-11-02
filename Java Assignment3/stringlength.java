63659da7da9ce25c27714e8ae31355d018eb26f2
PravinLourthurajS <92928920+PravinLourthurajS@users.noreply.github.com>
2021-11-02 13:33:19 +0530

    Add files via upload

---
+++ Java Assignment3/stringlength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+	
+	public static void main(String[] args) {
+		String str= "Hello World";
+		//This is for Q.1.1
+		System.out.println("The length of the string: "+ str.length());
+	}
+}
